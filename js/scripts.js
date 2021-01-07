1b3e3d4ffdbba596a235938ea3e5d18811058973
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2021-01-07 17:23:40 +0500

    add filter js

--- js/scripts.js
+++ js/scripts.js
@@ -31,16 +31,17 @@ if(elForm) {
   var elTitleInput = $_('.js-search-form__title-input', elForm);
   var elRatingInput = $_('.js-search-form__rating-input', elForm);
   var elGenreSelect = $_('.js-search-form__genre-select', elForm);
+  var elFiltreSelect = $_('.js-search-form__sort-select', elForm);
 }
 var elSearchResultCount = $_('.search-result-count');
 var elSearchResultParent = $_('.search-count-parent-element');
 var elOutputList = $_('.search-results');
-var elMovieOutputTemplate = $_('#search-result-template').content;
 var elBookmarkOutputList = $_('.bookmarked-movies');
 
 
 /* ==================== movie larni output qilish funksiyasi ========================= */
-var outputMovie = function(array) {
+var displayMovie = function(array) {
+  var elMovieOutputTemplate = $_('#search-result-template').content;
   elOutputList.innerHTML = '';
   var movieItemFragment = document.createDocumentFragment();
   for(var i = 0; i < array.length; i++) {
@@ -50,6 +51,7 @@ var outputMovie = function(array) {
     $_('.movie__year', newItem).textContent = array[i].year;
     $_('.movie__rating', newItem).textContent = array[i].imdb_rating;
     $_('.movie__poster', newItem).src = array[i].youtubeSmallThumbnail;
+    $_('.js-movie-bookmark', newItem).dataset.movieId = i;
 
     movieItemFragment.appendChild(newItem);
     elSearchResultParent.appendChild(elSearchResultCount);
@@ -58,6 +60,20 @@ var outputMovie = function(array) {
 }
 
 
+/* ===========  movie larni bookmarkga qoshish  ============ */
+// var addBookmark = function(array) {
+//   if(evt.target.match($_('.js-movie-bookmark', newItem))) {
+//     var bookmarkTemplate = $_('#bookmarked-movie-template').content;
+
+//     for(var i = 0; i < array.length; i++) {
+//       var newBookmarkItem = bookmarkTemplate.cloneNode(true);
+//       $_('.bookmarked-movie__title', newBookmarkItem).textContent = array[i].title;
+//     }
+//   }
+// }
+
+
+/* ===============  inputlarni clear qlish funksiyasi  ================ */
 var clearInputs = function() {
   elTitleInput.value = '';
   elRatingInput.value = '';
@@ -87,10 +103,97 @@ elForm.addEventListener('submit', function(evt) {
 
   var newInputValue = new RegExp(elTitleInput.value, 'gi');
 
+  if(elFiltreSelect.value === 'rating_desc') {
+    var selectedFiltr = movies.sort(function(a, b) {
+      if(a.imdb_rating < b.imdb_rating) {
+        return 1;
+      }
+      if(a.imdb_rating > b.imdb_rating) {
+        return -1;
+      }
+      return 0;
+    });
+    displayMovie(elFiltreSelect);
+  }
+  if(elFiltreSelect.value === 'az') {
+    var newSort = movies.sort(function(a, b) {
+      if(a.title > b.title) {
+        return 1;
+      }
+      if(a.title < b.title) {
+        return -1;
+      }
+      return 0;
+    });
+    displayMovie(newSort);
+  }
+  if(elFiltreSelect.value === 'za') {
+    var newSort = movies.sort(function(a, b) {
+      if(a.title < b.title) {
+        return 1;
+      }
+      if(a.title > b.title) {
+        return -1;
+      }
+      return 0;
+    });
+    displayMovie(newSort);
+  }
+  if(elFiltreSelect.value === 'year_desc') {
+    var newSort = movies.sort(function(a, b) {
+      if(a.year < b.year) {
+        return 1;
+      }
+      if(a.year > b.year) {
+        return -1;
+      }
+      return 0;
+    });
+    displayMovie(newSort);
+  }
+  if(elFiltreSelect.value === 'year_asc') {
+    var newSort = movies.sort(function(a, b) {
+      if(a.year > b.year) {
+        return 1;
+      }
+      if(a.year < b.year) {
+        return -1;
+      }
+      return 0;
+    });
+    displayMovie(newSort);
+  }
+  if(elFiltreSelect.value === 'rating_asc') {
+    var newSort = movies.sort(function(a, b) {
+      if(a.imdb_rating > b.imdb_rating) {
+        return 1;
+      }
+      if(a.imdb_rating < b.imdb_rating) {
+        return -1;
+      }
+      return 0;
+    });
+    displayMovie(newSort);
+  }
+
+
+
   var newTitleArr = movies.filter(function(movie) {
     var isAll = elGenreSelect.value === 'All' || movie.categories.includes(elGenreSelect.value);
-    return movie.title.toString().match(newInputValue) && movie.imdb_rating >= Number(elRatingInput.value) && isAll;
+    return movie.title.toString().match(newInputValue) && movie.imdb_rating >= Number(elRatingInput.value) && isAll && selectedFiltr || newSort;
   });
-  outputMovie(newTitleArr);
-  clearInputs();
+  displayMovie(newTitleArr);
+  // clearInputs();
+
+
+
+
 });
+
+
+
+if(elOutputList) {
+  elOutputList.addEventListener('click', function() {
+    addBookmark(newTitleArr);
+  });
+}
